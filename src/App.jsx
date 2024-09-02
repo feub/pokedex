3047d110627837f77e36a3ac5f56186a831b07b4
Fabien <fabien@feub.net>
2024-09-02 16:22:27 +0200

    Component PokemonCard: addednavigation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 
 const pokemonList = [
@@ -7,14 +8,47 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePrevious = () => {
+    if (pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (pokemonIndex < pokemonList.length - 1) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <div>
+        <button onClick={handlePrevious}>Précédent</button>
+        <button onClick={handleNext}>Suivant</button>
+      </div>
     </div>
   );
 }
