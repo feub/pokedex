d8f2e1c2658dfa609d4e1bb81ac4f2f727f3177f
Fabien <fabien@feub.net>
2024-09-02 16:06:32 +0200

    Component PokemonCard now with props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,6 @@
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
 
-export default function PokemonCard() {
-    const pokemon = pokemonList[0];
+
+export default function PokemonCard({ pokemon }) {
 
     return (
         <figure>
