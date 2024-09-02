bbc04cad6ea7aeb2318a5276c55a60e5c77d3c85
Fabien <fabien@feub.net>
2024-09-02 15:38:30 +0200

    Component PokemonCard dynamic

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,21 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
+
 export default function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc !== undefined ? <img src={pokemon.imgSrc} /> : <p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     );
 }
